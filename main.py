3b3970528870b48fcc8585e3b59c7ea0d097f434
Shuheng Johnny Liu <wish1104@outlook.com>
2019-05-29 12:34:12 +0800

    create main script for running a session with specified args

---
+++ main.py
@@ -0,0 +1,25 @@
+from torch.optim import Adam
+from torchvision.models.vgg import vgg16
+from image_utils import ImageLoader, ImageDumper
+from model import StyleNet
+from session import Session
+from projection import VggProjection
+from cli import CustomizedArgs, CustomizedParser
+
+if __name__ == '__main__':
+    parser = CustomizedParser()
+    args = CustomizedArgs(parser)
+
+    reference_model = vgg16(pretrained=True).features
+    projection_layer = VggProjection()
+    style_tensor = ImageLoader(path=args.style_path).tensor
+    content_image = ImageLoader(path=args.content_path)
+    content_tensor = content_image.tensor
+    styler = StyleNet(reference_model, projection_layer, content_tensor, style_tensor, [4], [1, 2, 3, 4])
+    styler.build_model()
+    session = Session(styler, Adam, alpha=args.alpha, n_steps=args.steps_per_epoch)
+
+    for epoch_count in range(1, args.n_epochs + 1):
+        print('starting epoch {}'.format(epoch_count))
+        result = session.epoch()
+        ImageDumper(result, 'output/output_{}.jpg'.format(epoch_count)).dump()
