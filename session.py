a18081db69b19c37e100ebaae80392e3fe589a62
Shuheng Johnny Liu <wish1104@outlook.com>
2019-05-31 04:15:44 +0800

    refactor signature of constructor to allow incremental training

--- session.py
+++ session.py
@@ -4,22 +4,29 @@ from global_utils import flush_json_metrics
 
 
 class Session:
-    def __init__(self, styler: StyleNet, optimizer_class, alpha=1e6, n_steps=300, from_scratch=False):
+    def __init__(self, styler: StyleNet, optimizer_class, alpha=1e6, n_steps=300, start_from="content"):
         """
         a session than returns pastiche each epoch (and each step)
         :param styler: a StyleNet with specified content and style
         :param optimizer_class: a subclass of torch.optim.optimizer.Optimizer, whose constructor will be used
         :param alpha: relative weight of style loss to content loss
         :param n_steps: number of steps per epoch
-        :param from_scratch: if True, pastiche is initialized to random noise; a clone of content image otherwise
+        :param start_from: if 'content', start from a clone of content
+                           if 'style', start from a clone of style
+                           if 'scratch', start from a clone of style
+                           if a torch.Tensor, start from the tensor
         """
         self.styler = styler
         self.metrics = {}
-        if from_scratch:
+        if start_from == "scratch":
             device = self.styler.content_tensor.device
             self._pastiche = torch.rand(*self.styler.style_tensor.shape, device=device).requires_grad_(True)
-        else:
+        elif start_from == "content":
             self._pastiche = self.styler.content_tensor.clone().requires_grad_(True)
+        elif start_from == "style":
+            self._pastiche = self.styler.style_tensor.clone().requires_grad_(True)
+        else:
+            self._pastiche = start_from.clone().requires_grad_(True)
         self.optimizer = optimizer_class([self._pastiche])
         self.alpha = alpha
         self._global_step = 0
